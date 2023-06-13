4207d1c1c243492238ea19c299fc1ce89ec292b1
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-06-13 18:32:35 +0530

    chore(deps): upgraded to wagmi 0.12.15

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,25 @@ import {
   useConnect,
   useSigner,
   useDisconnect,
+  mainnet,
+  useAccount,
+  useBalance,
 } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { multicall } from "@wagmi/core";
+import wagmiGotchiABI from "./contracts/wagmiGotchiABI.json";
+import mLootABI from "./contracts/mLootABI.json";
+
+const wagmigotchiContract = {
+  address: "0xecb504d39723b0be0e3a9aa33d646642d1051ee1",
+  abi: wagmiGotchiABI,
+};
+const mlootContract = {
+  address: "0x1dfe7ca09e99d10835bf73044a23b73fc20623df",
+  abi: mLootABI,
+};
 
 const CHAIN_ID = 97;
 const WALLET_CONNECT_PROJECT_ID = "3f930f8e56336b44761655d8a270144c";
@@ -43,18 +59,21 @@ const chain: Chain = {
   },
 };
 
-const { provider } = configureChains([chain], [publicProvider()]);
+const { provider } = configureChains([mainnet], [publicProvider()]);
 
 const client = createClient({
   autoConnect: true,
   provider,
   connectors: [
     new WalletConnectConnector({
-      chains: [chain],
+      chains: [mainnet],
       options: {
         projectId: WALLET_CONNECT_PROJECT_ID,
         showQrModal: true,
       },
+    }),
+    new MetaMaskConnector({
+      chains: [mainnet],
     }),
   ],
 });
@@ -63,13 +82,56 @@ function ConnectButton() {
   const { connectors, connectAsync } = useConnect();
   const { disconnectAsync } = useDisconnect();
   const { data: signer } = useSigner();
+  const { address } = useAccount();
+  const { data: balance } = useBalance({
+    address,
+  });
 
   const onConnect = () =>
-    connectAsync({ connector: connectors[0], chainId: CHAIN_ID });
+    connectAsync({ connector: connectors[0], chainId: 1 });
 
-  const fetchBalance = async () => {
-    const balance = signer?.getBalance();
+  const handleMulticall = async () => {
+    const data = await multicall({
+      contracts: [
+        {
+          ...wagmigotchiContract,
+          functionName: "getAlive",
+        },
+        {
+          ...wagmigotchiContract,
+          functionName: "getBoredom",
+        },
+        {
+          ...mlootContract,
+          functionName: "getChest",
+          args: [69],
+        },
+        {
+          ...mlootContract,
+          functionName: "getWaist",
+          args: [69],
+        },
+      ],
+    });
+    console.log(data);
+  };
+
+  const fetchBalance = () => {
     console.log("Balance", balance);
+    if (signer) {
+      signer.getAddress().then((address) => {
+        console.log("Address", address);
+      });
+      signer.getChainId().then((chainId) => {
+        console.log("ChainId", chainId);
+      });
+      signer.getGasPrice().then((gasPrice) => {
+        console.log("GasPrice", gasPrice);
+      });
+      signer.getBalance().then((balance) => {
+        console.log("Balance from signer", balance);
+      });
+    }
   };
 
   return (
@@ -77,6 +139,7 @@ function ConnectButton() {
       {signer ? (
         <>
           <button onClick={fetchBalance}>Fetch balance</button>
+          <button onClick={() => handleMulticall()}>MultiCall</button>
           <button onClick={() => disconnectAsync()}>Disconnect</button>
         </>
       ) : (
